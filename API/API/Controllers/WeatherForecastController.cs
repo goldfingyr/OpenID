c5d3730fcf75c222ab2070d5b9066cf77024d99c
Karsten Jeppesen <kaje@ucn.dk>
2023-01-27 18:48:46 +0100

    API: Working, verified
    Will work with
    Client Credentials Flow
    Resource Owner Password Flow

--- API/API/Controllers/WeatherForecastController.cs
+++ API/API/Controllers/WeatherForecastController.cs
@@ -1,9 +1,26 @@
+// Based on https://blog.devgenius.io/security-in-react-and-webapi-in-asp-net-core-c-with-authentification-and-authorization-by-keycloak-f890d340d093
+
+
+// One of the next two lines must be outcommented
+#define USE_ClientCredentialsFlow
+//#define USE_ResourceOwnerPasswordFlow
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace API.Controllers
 {
+
     [ApiController]
     [Route("[controller]")]
+#if (USE_ResourceOwnerPasswordFlow)
+    [Authorize(AuthenticationSchemes = "Bearer", Roles = "Admin")]
+#endif
+#if (USE_ClientCredentialsFlow)
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+#endif
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
