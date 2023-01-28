d7bcbd84c9b067bf8f32f9979d3d54d692024483
Karsten Jeppesen <kaje@ucn.dk>
2023-01-28 16:42:45 +0100

    Working solution

--- FormsApp-OidcClient/FormsApp-OidcClient/Form1.Designer.cs
+++ FormsApp-OidcClient/FormsApp-OidcClient/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtLogin = new System.Windows.Forms.Button();
+            this.TbResponse = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtLogin
+            // 
+            this.BtLogin.Location = new System.Drawing.Point(4, 6);
+            this.BtLogin.Name = "BtLogin";
+            this.BtLogin.Size = new System.Drawing.Size(102, 28);
+            this.BtLogin.TabIndex = 0;
+            this.BtLogin.Text = "Login";
+            this.BtLogin.UseVisualStyleBackColor = true;
+            this.BtLogin.Click += new System.EventHandler(this.BtLogin_Click);
+            // 
+            // TbResponse
+            // 
+            this.TbResponse.Location = new System.Drawing.Point(4, 39);
+            this.TbResponse.Multiline = true;
+            this.TbResponse.Name = "TbResponse";
+            this.TbResponse.Size = new System.Drawing.Size(792, 409);
+            this.TbResponse.TabIndex = 1;
+            this.TbResponse.Text = "Login response will be shown here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TbResponse);
+            this.Controls.Add(this.BtLogin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button BtLogin;
+        private TextBox TbResponse;
     }
 }
--- FormsApp-OidcClient/FormsApp-OidcClient/Form1.cs
+++ FormsApp-OidcClient/FormsApp-OidcClient/Form1.cs
@@ -1,10 +1,71 @@
+using FormsApp_OidcClient.Browser;
+using IdentityModel.OidcClient;
+using System.Text;
+
 namespace FormsApp_OidcClient
 {
     public partial class Form1 : Form
     {
+        OidcClient _oidcClient;
         public Form1()
         {
             InitializeComponent();
+            var options = new OidcClientOptions
+            {
+                //Authority = "https://demo.identityserver.io",
+                //Authority = "http://keycloak.l2.ucnit.eu/auth/realms/TheGlassHouse/",
+                //ClientId = "API",
+                //ClientSecret = "2de46b9d-0afa-469f-b7a3-3d57c453888c",
+                //Scope = "email profile roles",
+                //RedirectUri = "https://localhost/winforms.client",
+                Authority = System.Environment.GetEnvironmentVariable("AuthenticationServer"),
+                ClientId = System.Environment.GetEnvironmentVariable("ClientId"),
+                ClientSecret = System.Environment.GetEnvironmentVariable("ClientSecret"),
+                Scope = System.Environment.GetEnvironmentVariable("Scope"),
+                RedirectUri = System.Environment.GetEnvironmentVariable("RedirectURI"),
+                Browser = new WinFormsWebView()
+            };
+            // IdentityModel.OidcClient will not accept self signed certificates
+            options.Policy.Discovery.RequireHttps = false;
+            _oidcClient = new OidcClient(options);
+        }
+
+        private async void BtLogin_Click(object sender, EventArgs e)
+        {
+            var result = await _oidcClient.LoginAsync();
+
+            if (result.IsError)
+            {
+                MessageBox.Show(this, result.Error, "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var sb = new StringBuilder(128);
+                foreach (var claim in result.User.Claims)
+                {
+                    sb.AppendLine($"{claim.Type}: {claim.Value}");
+                }
+
+                if (!string.IsNullOrWhiteSpace(result.RefreshToken))
+                {
+                    sb.AppendLine();
+                    sb.AppendLine($"refresh token: {result.RefreshToken}");
+                }
+
+                if (!string.IsNullOrWhiteSpace(result.IdentityToken))
+                {
+                    sb.AppendLine();
+                    sb.AppendLine($"identity token: {result.IdentityToken}");
+                }
+
+                if (!string.IsNullOrWhiteSpace(result.AccessToken))
+                {
+                    sb.AppendLine();
+                    sb.AppendLine($"access token: {result.AccessToken}");
+                }
+
+                TbResponse.Text = sb.ToString();
+            }
         }
     }
 }
